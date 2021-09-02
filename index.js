8c421b37b9fe9fcf40796b76233a7d6a407528a2
ShiniDev <markjoefreylaurente@gmail.com>
2021-09-02 17:27:07 +0800

    Server is now able to server files respective to file structure Server can now detect specific mime types

--- index.js
+++ index.js
@@ -5,21 +5,37 @@ const http = require('http');
 const url = require('url');
 
 // Set Global Variables
-process.env.ROOT_DIRECTORY = process.cwd() + "/";
-process.env.PUBLIC_DIRECTORY = process.env.ROOT_DIRECTORY + "public/";
+process.env.ROOT_DIRECTORY = process.cwd();
+process.env.PUBLIC_DIRECTORY = process.env.ROOT_DIRECTORY + "/public";
 
 // Modules
+const fileFunctions = require('./server/file-functions');
 
 // App Settings
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     const HTTP_METHOD = req.method;
-    if (HTTP_METHOD == "GET") {
+    if (HTTP_METHOD === "GET") {
         const URL = new url.URL(`http://${req.headers.host}${req.url}`);
+        const result = fileFunctions.readFile(URL.pathname);
+        const filePath = fileFunctions.updatePath(URL.pathname);
+        result.then(result => {
+            if (result === "error404" || result === undefined) {
+                res.writeHead(404, { "Content-type": "text/html" });
+                fileFunctions.readFile("/errors/error-404.html").then(result => {
+                    res.end(result);
+                })
+            } else {
+                res.writeHead(200, { "Content-type": fileFunctions.fileContentType(filePath) })
+                res.end(result);
+            }
+        });
     } else {
         res.writeHead(405, { "Content-type": "text/html" });
-        res.end('Test');
+        fileFunctions.readFile("/errors/error-405.html").then(result => {
+            res.end(result);
+        });
     }
 });
 
--- server/file-functions.js
+++ server/file-functions.js
@@ -1,15 +1,54 @@
 "use strict";
 
 const fs = require('fs').promises;
-
+const { lstatSync, existsSync } = require('fs');
+const { join, extname } = require('path');
 
 const checkPath = (filePath) => {
+    filePath = join(process.env.PUBLIC_DIRECTORY, filePath);
+    if (!existsSync(filePath)) {
+        return "error404";
+    }
+    let isDirectory = lstatSync(filePath).isDirectory();
+    if (isDirectory) {
+        if (filePath.charAt(filePath.length - 1) !== "/") {
+            filePath = `${filePath}/`;
+        }
+        filePath = `${filePath}index.html`;
+    }
+    if (existsSync(filePath)) {
+        return filePath;
+    } else {
+        return "error404";
+    }
+    // return filePath;
+};
 
+const readFilePromise = async (filePath) => {
+    return await fs.readFile(filePath, 'utf-8');
 };
 
 module.exports = {
-    readFile: async function (filePath) {
-        const fileContents = await fs.readFile(filePath, 'utf-8');
+    updatePath: filePath => checkPath(filePath),
+    readFile: (filePath) => {
+        filePath = checkPath(filePath);
+        if (filePath === "error404") {
+            return Promise.resolve("error404");
+        }
+        const fileContents = readFilePromise(filePath);
         return fileContents;
+    },
+    fileContentType: (filePath) => {
+        let fileType = extname(filePath);
+        switch (fileType) {
+            case ".html":
+                return "text/html";
+            case ".css":
+                return "text/css";
+            case ".js":
+                return "text/javascript";
+            default:
+                return "text/plain";
+        }
     }
 };
